51101443bd3d53e4f35192fc4a41478ea7a97aa8
EricNnaike <83033704+EricNnaike@users.noreply.github.com>
2021-12-07 14:05:19 +0100

    second commit
    Variables changed

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-var reg1 = /[a-z]+/g;
-var reg2 = /[A-Z]+/g;
-var reg3 = /[0-9]+/g;  
-var reg4 = /[$@#&!]+/g;
+var reg1 = /([a-z][A-Z][0-9][$@#&!])?/g;
+var reg2 = /[a-z]{1}[A-Z0-9$@#&!]{1}/g;
+var reg3 = /[a-z]{1}[A-Z0-9$@#&!]{2}/g;  
+var reg4 = /[a-z][A-Z][0-9][$@#&!]/g;  
 // let password = 'wA6uche$';
 let myInput = document.getElementById('pwd');
-var submitOk = "true";
 var text;
 
 function passVerification() {
     let inputFig = String(myInput.value);
 
-    if(inputFig.match(reg1) || inputFig.match(reg2) || inputFig.match(reg3) || inputFig.match(reg4)) {
+    if(inputFig.match(reg1)) {
         text = 'Password strength: ' + 25 + '%';
+    } else{
+        text = 'too much';
     }
 
     if(inputFig.match(reg2)) {
         text = 'Password strength: ' + 50 + '%';
-    } 
+    }
 
     if(inputFig.match(reg3)){
         text = 'Password strength: ' + 75 + '%';
     }
 
-    if(inputFig.match(reg1) && inputFig.match(reg2) && inputFig.match(reg3) && inputFig.match(reg4)){
+    if(inputFig.match(reg4)){
         text = 'Password strength: ' + 100 + '%';
     }
 
     if(inputFig.length < 6) {
         text = 'Minimum character requirement is 6';
     } 
-
-    // if(submitOk == 'false') {
-    //     return false;
-    //  }
 
      document.getElementById('textIn').innerHTML = text;
 }
