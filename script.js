64c20f019b6ee4042ee97251447be4534dd2b547
EricNnaike <83033704+EricNnaike@users.noreply.github.com>
2021-12-09 12:30:04 +0100

    Password reset

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-var reg1 = /([a-z][A-Z][0-9][$@#&!])?/g;
-var reg2 = /[a-z]{1}[A-Z0-9$@#&!]{1}/g;
-var reg3 = /[a-z]{1}[A-Z0-9$@#&!]{2}/g;  
-var reg4 = /[a-z][A-Z][0-9][$@#&!]/g;  
-// let password = 'wA6uche$';
 let myInput = document.getElementById('pwd');
-var text;
 
-function passVerification() {
-    let inputFig = String(myInput.value);
+function validatePassword() {
 
-    if(inputFig.match(reg1)) {
-        text = 'Password strength: ' + 25 + '%';
-    } else{
-        text = 'too much';
+    let password = String(myInput.value);
+
+    // if (password.length < 6) {
+    //     return "Minimum password length is 6";
+    // }
+
+    // if (password.length > 12) {
+    //     return "Maximum password length is 12";
+    // }
+
+    let passwordStrength = 0;
+
+    if (/[a-z]/.test(password)) {
+        passwordStrength += 25;
     }
 
-    if(inputFig.match(reg2)) {
-        text = 'Password strength: ' + 50 + '%';
+    if (/[A-Z]/.test(password)) {
+        passwordStrength += 25;
     }
 
-    if(inputFig.match(reg3)){
-        text = 'Password strength: ' + 75 + '%';
+    if (/[0-9]/.test(password)) {
+        passwordStrength += 25;
     }
 
-    if(inputFig.match(reg4)){
-        text = 'Password strength: ' + 100 + '%';
+    if (/[$@#&!]/.test(password)) {
+        passwordStrength += 25;
     }
 
-    if(inputFig.length < 6) {
-        text = 'Minimum character requirement is 6';
-    } 
+    // return '${passwordStrength}%';
 
-     document.getElementById('textIn').innerHTML = text;
+    document.getElementById('textIn').innerHTML = passwordStrength;
 }
+
+/*  */
